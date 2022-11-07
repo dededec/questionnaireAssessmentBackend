139b9bbc10659f0bb7ed91c52e262c55e4b7ba0a
dededec <daviderenacasado@gmail.com>
2022-11-07 10:17:46 +0100

    First commit

---
+++ src/main/java/com/migueldavid/QuestionnaireAssessmentBackend/QuestionnaireAssessmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.migueldavid.QuestionnaireAssessmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireAssessmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireAssessmentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migueldavid/QuestionnaireAssessmentBackend/QuestionnaireAssessmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migueldavid.QuestionnaireAssessmentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireAssessmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
