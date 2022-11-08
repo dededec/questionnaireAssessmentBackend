0482a140f0639b415d4b2853888d71a132d4a773
dededec <daviderenacasado@gmail.com>
2022-11-08 09:30:16 +0100

    Question Controller created.

--- src/main/java/com/migueldavid/QuestionnaireAssessmentBackend/controllers/QuestionController.java
+++ src/main/java/com/migueldavid/QuestionnaireAssessmentBackend/controllers/QuestionController.java
@@ -3,8 +3,11 @@ package com.migueldavid.QuestionnaireAssessmentBackend.controllers;
 import com.migueldavid.QuestionnaireAssessmentBackend.models.entities.Question;
 import com.migueldavid.QuestionnaireAssessmentBackend.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class QuestionController {
@@ -12,18 +15,24 @@ public class QuestionController {
     @Autowired
     private QuestionService service;
 
-    @PostMapping(path = "/add")
+    @PostMapping(path = "/question/add")
     private ResponseEntity<Question> createQuestion(@RequestBody Question question){
-        return null;
+        Question saved = service.addQuestion(question);
+        if(saved == null){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        }
     }
 
-    @GetMapping(path = "/get/{id}")
-    private ResponseEntity<Question> getQuestion(@PathVariable int id){
-        return null;
-    }
+//    @GetMapping(path = "/question/get/{id}")
+//    private ResponseEntity<Question> getQuestion(@PathVariable int id){
+//        return new ResponseEntity<>(service., HttpStatus.BAD_REQUEST);
+//    }
 
-    @GetMapping(path = "/get/all")
-    private ResponseEntity<Question> getAllQuestion(){
-        return null;
+    @GetMapping(path = "/question/get/all")
+    private ResponseEntity<List<Question>> getAllQuestion(){
+        return new ResponseEntity<>(service.getQuestions(), HttpStatus.OK);
     }
 }
